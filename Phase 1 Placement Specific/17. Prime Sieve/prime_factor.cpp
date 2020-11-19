66b084eecbec85f53d5013bad572e6fbb28b5fcd
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-19 10:36:16 +0530

    updated code for lecture 17

--- Phase 1 Placement Specific/17. Prime Sieve/prime_factor.cpp
+++ Phase 1 Placement Specific/17. Prime Sieve/prime_factor.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
-int main(){
+void primeFactor(int n){
 
-        
+    int spf[n+1] = {0};
+    for(int i = 2; i<=n; i++){
+        spf[i] = i;        
+    }
+    for(int i = 2; i<=n; i++){
+        if(spf[i] == i){
+            for(int j = i*i; j<=n; j+=i){
+                if(spf[j] == j)
+                    spf[j] = i;
+            }
+        }
+    }
+
+    while(n != 1){
+
+                    cout<<spf[n]<<" ";
+                    n /= spf[n];
+            }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    primeFactor(n);        
 
     return 0;
 }
--- Phase 1 Placement Specific/17. Prime Sieve/prime_no.cpp
+++ Phase 1 Placement Specific/17. Prime Sieve/prime_no.cpp
@@ -2,9 +2,28 @@
 
 using namespace std;
 
-int main(){
+void primeSieve(int n){
+    int flag[n+1] = {0};
+    for(int i = 2; i<=n; i++){
+        if(flag[i] == 0){
+            for(int j = i*i; j<=n; j+=i){
+                flag[j] = 1;
+            }
+        }
+    }
 
-        
+    for(int j = 2; j<=n; j++){
+                if(flag[j] == 0)
+                    cout<<j<<" ";
+            }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    primeSieve(n);        
 
     return 0;
 }

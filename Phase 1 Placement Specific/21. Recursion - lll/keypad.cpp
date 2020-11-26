01cf384cae1677492d5d1ca3abeaa59a004813ad
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-26 21:32:14 +0530

    added soln of lecture 21 subset of string ASCII code

---
+++ Phase 1 Placement Specific/21. Recursion - lll/keypad.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void subseq(string s,string ans){
+    if(s.length() == 0){
+        cout<<ans<<endl;
+        return;
+    }
+    char ch = s[0];
+    int code = ch;
+    string ros = s.substr(1);
+
+    subseq(ros,ans);
+    subseq(ros,ans+ch);
+    subseq(ros,ans+to_string(code));
+}
+
+int main(){
+    subseq("ab","");
+    return 0;
+}

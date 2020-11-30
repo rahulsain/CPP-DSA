b21f5853ef0a8449f3b8103f9965578373ba9e9c
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-11-30 17:35:19 +0530

    rename some files

---
+++ Phase 1 Placement Specific/21. Recursion - lll/21.1 Recursion 1o1/permutation_string.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void permutation(string s,string ans){
+    if(s.length() == 0){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i = 0; i<s.length(); i++){
+        char ch = s[i];
+    string ros = (s.substr(0,i) + s.substr(i+1));
+    
+    permutation(ros,ans+ch);
+}
+}
+
+int main(){
+
+    string n;
+    cin>>n;
+
+    permutation(n,"");    
+
+    return 0;
+}

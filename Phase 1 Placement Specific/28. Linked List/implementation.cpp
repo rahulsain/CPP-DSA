9603ef2b80a016bfb69681ddf3c8ca548d788a7d
rahulsain3000 <50570677+rahulsain@users.noreply.github.com>
2020-12-18 15:13:43 +0530

    updated the implement funct till linear search

--- Phase 1 Placement Specific/28. Linked List/implementation.cpp
+++ Phase 1 Placement Specific/28. Linked List/implementation.cpp
@@ -12,6 +12,12 @@ class node{
     }
 };
 
+void insertAtHead(node* &head,int data){
+    node* temp = new node(data);
+    temp->next = head;
+    head = temp;
+}
+
 void insertAtTail(node* &head,int data){
     node* temp = new node(data);
     if(head == NULL){
@@ -23,6 +29,26 @@ void insertAtTail(node* &head,int data){
         temp = temp->next;
     }
     temp->next = temp;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+bool linearSearch(node* &head,int key){
+    node* temp = head;
+    while(temp != NULL){
+        if(temp->data == key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
 }
 
 int main(){
